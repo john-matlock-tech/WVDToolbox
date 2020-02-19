49edc8b7fe9baf9a45c3a4357b9a259b868f273a
john-matlock-tech <60303867+john-matlock-tech@users.noreply.github.com>
2020-02-19 07:54:30 -0800

    Support updates for publish
    support updates for publish

--- src/WVDToolbox/Public/Publish-WVDHostPool.ps1
+++ src/WVDToolbox/Public/Publish-WVDHostPool.ps1
@@ -32,15 +32,25 @@ function Publish-WVDHostPool
             HelpMessage = 'Host Pool name.')]
         [ValidateNotNull()]
         [ValidateNotNullOrEmpty()]
-        [string]$Name
+        [string]$Name,
+
+        [Parameter(Mandatory = $false,
+            HelpMessage = 'Indicates that the update template should be used.')]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        [switch]$Update
     )
 
 
-        $config = Get-WVDHostPool -environment $Environment -Name $Name
-        $envConf = Get-WVDEnvironment $Environment
-        Connect-AzAccount
-        Connect-WVDEnvironment -DisplayName $Environment
+    $config = Get-WVDHostPool -environment $Environment -Name $Name
+    $envConf = Get-WVDEnvironment $Environment
+    Connect-AzAccount
+    Connect-WVDEnvironment -DisplayName $Environment
 
+
+
+    if ($Update -like $false)
+    {
         $paramObject = @{
             'rdshImageSource' = $config.ImageSource
             'rdshCustomImageSourceName'  = $config.ImageSourceName
@@ -72,7 +82,44 @@ function Publish-WVDHostPool
             'TemplateFile' = "$PSScriptRoot\..\Private\WVD\DeployHostPoolTemplate.json"
             'TemplateParameterObject'    = $paramObject
         }
+    }
+    else
+    {
+        $paramObject = @{
+            'rdshImageSource' = $config.ImageSource
+            'rdshCustomImageSourceName'  = $config.ImageSourceName
+            'rdshCustomImageSourceResourceGroup' = $config.ImageResourceGroup
+            'rdshNamePrefix' = $config.HostNamePrefix
+            'rdshNumberOfInstances' = $config.HostCount
+            'rdshVMDiskType' = 'Premium_LRS'
+            'rdshVmSize' = $config.HostSize
+            'domainToJoin' = $config.DomainToJoin
+            'existingDomainUPN' = $config.DomainJoinUsername
+            'existingDomainPassword' = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR(($config.DomainJoinPassword | ConvertTo-SecureString)))
+            'ouPath' = $config.HostParentOU
+            'existingVnetName' = $config.VNetName
+            'newOrExistingVnet' = 'existing'
+            'existingSubnetName' = $config.SubnetName
+            'virtualNetworkResourceGroupName' = $config.VNETResourceGroup
+            'existingTenantGroupName' = $config.WVDTenantGroupName
+            'existingTenantName' = $config.WVDTenant
+            'existingHostPoolName' = $config.Name
+            'enablePersistentDesktop' = $config.EnablePersistentDesktop
+            'tenantAdminUpnOrApplicationId' = $envConf.UserName
+            'tenantAdminPassword' = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR(($envConf.Secret | ConvertTo-SecureString)))
+            'isServicePrincipal' = $true
+            'aadTenantId' = $envConf.AzureTenantId
+            'location' = 'westus2'
+            'userLogoffDelayInMinutes' = 0
+            'userNotificationMessage' = 'Scheduled maintenance, please save your work and logoff as soon as possible'
+        }
 
-        New-AzResourceGroupDeployment @params -verbose
+        $params = @{
+            'ResourceGroupName' = $config.ResourceGroup
+            'TemplateFile' = "$PSScriptRoot\..\Private\WVD\UpdateHostPoolTemplate.json"
+            'TemplateParameterObject'    = $paramObject
+        }
     }
 
+        New-AzResourceGroupDeployment @params -verbose
+}
